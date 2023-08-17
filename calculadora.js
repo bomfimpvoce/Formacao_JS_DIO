f2ea764632fc6045a91a479d7cda22b1201d2308
Felipe Bomfim <139933431+bomfimpvoce@users.noreply.github.com>
2023-08-17 02:32:11 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+
+const precoCombustivel = 5.50;
+const mediaKmPorLitro = 10;
+const distancia = 230;
+
+const litrosGastos = distancia / mediaKmPorLitro;
+const custoViagem = litrosGastos * precoCombustivel;
+
+console.log();
